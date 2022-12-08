2fbef09e314f8ebb3746c3f77da04af120fec6da
giuliof <giulio@glgprograms.it>
2022-12-08 18:04:22 +0100

    [encoder] use in-hardware quadrature encoder system instead of software

--- firmware/src/encoder.cpp
+++ firmware/src/encoder.cpp
@@ -4,27 +4,43 @@
 
 #include "util/port.h"
 
+#include "debug.h"
+
 /* * * * * * * * * * * * * * * * PUBLIC MEMBERS * * * * * * * * * * * * * * * */
 
-#define ENC_A B, 2
-#define ENC_B B, 1
+#define ENC_A B, 1
+#define ENC_B B, 2
 #define BTN_PIN B, 3
 
-static uint8_t old_encoder_status;
+static uint8_t old_encoder_pos = 0;
 static bool old_btn_status;
 
 /* * * * * * * * * * * * * * SHARED IMPLEMENTATIONS * * * * * * * * * * * * * */
 
 void encoder_init()
 {
-  Port(ENC_A).DIRCLR |= (PinMsk(ENC_A) | PinMsk(ENC_B));
+  // Configure encoder pushbutton as input with pull-up
   Port(BTN_PIN).DIRCLR |= PinMsk(BTN_PIN);
-  PinCtrl(ENC_A) = PORT_OPC_PULLUP_gc;
-  PinCtrl(ENC_B) = PORT_OPC_PULLUP_gc;
   PinCtrl(BTN_PIN) = PORT_OPC_PULLUP_gc;
-
-  old_encoder_status = Port(ENC_A).IN & (PinMsk(ENC_A) | PinMsk(ENC_B));
   old_btn_status = Port(BTN_PIN).IN & PinMsk(BTN_PIN);
+
+  // Configure encoder phases pin
+  Port(ENC_A).DIRCLR |= (PinMsk(ENC_A) | PinMsk(ENC_B));
+  // Set ENC_A and ENC_B as low level sense
+  PinCtrl(ENC_A) = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
+  PinCtrl(ENC_B) = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
+  // Set ENC_A as mux input for event channel 0
+  // TODO pin port and number is hardcoded
+  EVSYS.CH0MUX = EVSYS_CHMUX_PORTB_PIN1_gc;
+  // Enable quadrature decoding and digital filter
+  EVSYS.CH0CTRL = EVSYS_QDEN_bm | EVSYS_DIGFILT_8SAMPLES_gc;
+  // Set quadrature decoder as action for TCC (timer/counter C), source from Event ch0
+  TCC0.CTRLD = TC_EVACT_QDEC_gc | TC_EVSEL_CH0_gc;
+  // Set period and counter register (use counter as 8 bit)
+  TCC0.PER = 0xFF;
+  TCC0.CNT = 0x00;
+  // Enable TCC without prescaling
+  TCC0.CTRLA = TC_CLKSEL_DIV1_gc;
 }
 
 bool encoder_clicked()
@@ -43,17 +59,14 @@ bool encoder_clicked()
 
 int8_t encoder_update()
 {
-  int8_t inc = 0;
-  uint8_t status = Port(ENC_A).IN & (_BV(PinNo(ENC_A)) | _BV(PinNo(ENC_B)));
-  if (((status ^ old_encoder_status) == _BV(PinNo(ENC_B))) && !(status & _BV(PinNo(ENC_B))))
-  {
-    if (status & _BV(PinNo(ENC_A)))
-      inc++;
-    else
-      inc--;
-  }
+  uint8_t pos = TCC0.CNT;
 
-  old_encoder_status = status;
+  // One encoder tick adds 2 counter steps
+  int8_t delta = (pos - old_encoder_pos) >> 1;
 
-  return inc;
+  // Update last position only if a tick is detected
+  if (delta != 0)
+    old_encoder_pos = pos;
+
+  return delta;
 }
